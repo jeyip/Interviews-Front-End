66990d1f8c91d545cc4caa620c42301ac4e22757
jeyip <jeremy.yip7@gmail.com>
2016-08-05 10:10:05 -0700

    Created radio buttons for search filter.

--- react-petlist/src/components/presentational/SearchFilters.jsx
+++ react-petlist/src/components/presentational/SearchFilters.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 
-function SearchFilters() {
+function SearchFilters({ toggleAllFilter, toggleBoardingFilter, toggleSittingFilter }) {
   return (
     <div id="searchFilters">
       <div>
-        <input type="checkbox"></input>
+        <input name="searchFilter" type="radio" onChange={toggleAllFilter}></input>
+        <p>All</p>
+      </div>
+      <div>
+        <input name="searchFilter" type="radio" onChange={toggleBoardingFilter}></input>
         <p>Boarding</p>
       </div>
       <div>
-        <input type="checkbox"></input>
+        <input name="searchFilter" type="radio" onChange={toggleSittingFilter}></input>
         <p>Sitting</p>
       </div>
     </div>
   );
+}
+
+SearchFilters.propTypes = {
+  toggleAllFilter: React.PropTypes.func,
+  toggleBoardingFilter: React.PropTypes.func,
+  toggleSittingFilter: React.PropTypes.func,
 }
 
 module.exports = SearchFilters;
